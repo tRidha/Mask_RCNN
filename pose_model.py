627841417895dfd7eb62c7af68a5a343347741f1
tRidha <ridha.thariq@gmail.com>
2020-03-15 20:24:02 -0700

    Cleaned up code and added random minibatches

--- pose_model.py
+++ pose_model.py
@@ -1,3 +1,9 @@
+'''
+Written by Thariq Ridha, Marco De Leon, Muhammad Aadil Khan
+Some code also retieved from various sources:
+- Mask RCNN code from: https://github.com/matterport/Mask_RCNN
+- Learned to visualize model output (3d pose) on 2d image from: https://www.kaggle.com/zstusnoopy/visualize-the-location-and-3d-bounding-box-of-car/notebook
+'''
 import os
 import sys
 import random
@@ -15,7 +21,7 @@ import tensorflow_graphics.geometry.transformation.quaternion as tfq
 from PIL import ImageDraw, Image
 import cv2
 
-class_names = []
+CAR_ID = 3
 rcnn_model = 0 
 
 IMAGE_PATH = '../dataset/images/'
@@ -65,26 +71,8 @@ def init_maskrcnn():
   # Load weights trained on MS-COCO
   rcnn_model.load_weights(COCO_MODEL_PATH, by_name=True)
 
-  # COCO Class names
-  # Index of the class in the list is its ID. For example, to get ID of
-  # the teddy bear class, use: class_names.index('teddy bear')
-  class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
-                 'bus', 'train', 'truck', 'boat', 'traffic light',
-                 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
-                 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
-                 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
-                 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-                 'kite', 'baseball bat', 'baseball glove', 'skateboard',
-                 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
-                 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-                 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-                 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
-                 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
-                 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
-                 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
-                 'teddy bear', 'hair drier', 'toothbrush']
-
 # ---------------------------------------- Helper functions for training ---------------------------------------- #
+# Camera intrinsic parameters and transformation matrix
 fx = 2304.5479
 fy = 2305.8757
 cx = 1686.2379
@@ -94,6 +82,10 @@ cam_matrix = np.array([[fx, 0, cx],
               [0, 0, 1]], dtype=np.float32)
 
 def pose_to_pixel(x, y, z):
+	'''
+	Given x, y, z coordinates in 3D space, returns x, y coordinates
+	in 2D space (specific to camera used in dataset)
+	'''
   R = np.array([[1, 0, 0, 0],
                 [0, 1, 0, 0,],
                 [0, 0, 1, 0]])
@@ -105,22 +97,29 @@ def pose_to_pixel(x, y, z):
   return p_z
 
 def load_Y_values(csv_filename):
-  # Given a csv file, will return a Y matrix containing all the pose information
-  # associated with each training example as well as a list of filenames
+  ''' 
+  Given a csv file, will return a Y matrix containing all the pose information
+  associated with each training example as well as a list of filenames
+  '''
   with open(csv_filename, newline='') as csvfile:
     filenames = []
     reader = csv.reader(csvfile)
     data = list(reader)[1:]
     file_examples = []
+
+    # for each image
     for i in range(len(data)):
       list_of_params = data[i][1].split()
       examples = []
       k = 0
+
+      # for each car in the image
       while k < len(list_of_params):
         pose = list_of_params[k+1:k+7]
         pose = [float(i) for i in pose]
         translation = pose[3:]
         eulers = pose[:3]
+
         # parameters are ordered roll, pitch, yaw (input dataset is yaw, pitch, roll)
         quaternion_rot = list(euler2quat(eulers[2], eulers[1], eulers[0]))
         examples.append(quaternion_rot + translation)
@@ -133,10 +132,11 @@ def load_Y_values(csv_filename):
     return file_examples, filenames
 
 def extract_bounding_box_info(rcnn_model, filenames, file_examples, show_images = False):
-  # Given a list of images, runs each image through the trained rcnn_model
-  # to output a corresponding list of bounding box information for the
-  # car closest to the camera for each image.
-  car_class_id = class_names.index('car')
+  '''
+  Given a list of images, runs each image through the trained rcnn_model
+  to output a corresponding list of bounding box information for the
+  car closest to the camera for each image.
+  '''
   X = np.zeros((len(filenames),1028))
 
   x_train = []
@@ -183,7 +183,7 @@ def extract_bounding_box_info(rcnn_model, filenames, file_examples, show_images 
         if i in seen_cars:
           continue
         index = rois[i][1]
-        if r['class_ids'][index] == car_class_id:
+        if r['class_ids'][index] == CAR_ID:
           y1,x1,y2,x2 = rois[i][0]
 
           # normalize
@@ -211,99 +211,9 @@ def extract_bounding_box_info(rcnn_model, filenames, file_examples, show_images 
     print("Processed image " + str(k) + " and " + str(len(x_train)) + " cars.")
     
   X = np.asarray(x_train).T
-  Y = np.asarray(y_train).T
-          
-        
+  Y = np.asarray(y_train).T        
 
   return X, Y
-
-# ---------------------------------------- Displaying Pose in 2D Image ---------------------------------------- #
-# Code largely pulled from this notebook:
-# https://www.kaggle.com/zstusnoopy/visualize-the-location-and-3d-bounding-box-of-car
-
-# convert euler angle to rotation matrix
-def euler_to_Rot(yaw, pitch, roll):
-    Y = np.array([[cos(yaw), 0, sin(yaw)],
-                  [0, 1, 0],
-                  [-sin(yaw), 0, cos(yaw)]])
-    P = np.array([[1, 0, 0],
-                  [0, cos(pitch), -sin(pitch)],
-                  [0, sin(pitch), cos(pitch)]])
-    R = np.array([[cos(roll), -sin(roll), 0],
-                  [sin(roll), cos(roll), 0],
-                  [0, 0, 1]])
-    return np.dot(Y, np.dot(P, R))
-
-def draw_line(image, points):
-    color = (255, 0, 0)
-    cv2.line(image, tuple(points[1][:2]), tuple(points[2][:2]), color, 4)
-    cv2.line(image, tuple(points[1][:2]), tuple(points[4][:2]), color, 4)
-
-    cv2.line(image, tuple(points[1][:2]), tuple(points[5][:2]), color, 4)
-    cv2.line(image, tuple(points[2][:2]), tuple(points[3][:2]), color, 4)
-    cv2.line(image, tuple(points[2][:2]), tuple(points[6][:2]), color, 4)
-    cv2.line(image, tuple(points[3][:2]), tuple(points[4][:2]), color, 4)
-    cv2.line(image, tuple(points[3][:2]), tuple(points[7][:2]), color, 4)
-
-    cv2.line(image, tuple(points[4][:2]), tuple(points[8][:2]), color, 4)
-    cv2.line(image, tuple(points[5][:2]), tuple(points[8][:2]), color, 4)
-
-    cv2.line(image, tuple(points[5][:2]), tuple(points[6][:2]), color, 4)
-    cv2.line(image, tuple(points[6][:2]), tuple(points[7][:2]), color, 4)
-    cv2.line(image, tuple(points[7][:2]), tuple(points[8][:2]), color, 4)
-    return image
-
-
-def draw_points(image, points):
-    image = np.array(image)
-    for (p_x, p_y, p_z) in points:
-        # print("p_x, p_y", p_x, p_y)
-        cv2.circle(image, (p_x, p_y), 5, (255, 0, 0), -1)
-    return image
-
-def visualize_poses(img, poses):
-  # poses = np.asarray([0.140419, 0.1], -0.0224375, -3.07343, -5.83187, 4.71606, 20.5524])
-  car_poses = []
-
-  for pose in poses:
-    quat = pose[:4]
-    quat = quat / np.linalg.norm(quat)
-    roll, pitch, yaw = quat2euler(*quat)
-
-    car_poses.append((yaw, pitch, roll, pose[4], pose[5], pose[6]))
-
-  x_l = 1.02
-  y_l = 0.80
-  z_l = 2.31
-  for yaw, pitch, roll, x, y, z in car_poses:
-      # I think the pitch and yaw should be exchanged
-      yaw, pitch, roll = -pitch, -yaw, -roll
-      Rt = np.eye(4)
-      t = np.array([x, y, z])
-      Rt[:3, 3] = t
-      Rt[:3, :3] = euler_to_Rot(yaw, pitch, roll).T
-      Rt = Rt[:3, :]
-      P = np.array([[0, 0, 0, 1],
-                    [x_l, y_l, -z_l, 1],
-                    [x_l, y_l, z_l, 1],
-                    [-x_l, y_l, z_l, 1],
-                    [-x_l, y_l, -z_l, 1],
-                    [x_l, -y_l, -z_l, 1],
-                    [x_l, -y_l, z_l, 1],
-                    [-x_l, -y_l, z_l, 1],
-                    [-x_l, -y_l, -z_l, 1]]).T
-
-      img_cor_points = np.dot(cam_matrix, np.dot(Rt, P))
-      img_cor_points = img_cor_points.T
-      img_cor_points[:, 0] /= img_cor_points[:, 2]
-      img_cor_points[:, 1] /= img_cor_points[:, 2]
-      img_cor_points = img_cor_points.astype(int)
-      img = draw_points(img, img_cor_points)
-      img = draw_line(img, img_cor_points)
-      
-  img = Image.fromarray(img)
-  plt.imshow(img)
-  plt.show()
 
 # ---------------------------------------- Model Implementation ---------------------------------------- #
 import tensorflow as tf
@@ -314,13 +224,50 @@ def tf_rad2deg(rad):
   return rad / pi_on_180
 
 def create_placeholders(n_x, n_y):
-
-   
     X = tf.placeholder(dtype = tf.float32, shape=[n_x, None], name ='X')
     Y = tf.placeholder(dtype = tf.float32, shape=[n_y, None], name ='Y')
    
-    
     return X, Y
+
+def random_mini_batches(X, Y, mini_batch_size = 64):
+    """
+    From coursera
+    Creates a list of random minibatches from (X, Y)
+    
+    Arguments:
+    X -- input data, of shape (input size, number of examples)
+    Y -- true "label" vector (containing 0 if cat, 1 if non-cat), of shape (1, number of examples)
+    mini_batch_size - size of the mini-batches, integer
+    seed -- this is only for the purpose of grading, so that you're "random minibatches are the same as ours.
+    
+    Returns:
+    mini_batches -- list of synchronous (mini_batch_X, mini_batch_Y)
+    """
+    
+    m = X.shape[1]                  # number of training examples
+    mini_batches = []
+    
+    # Step 1: Shuffle (X, Y)
+    permutation = list(np.random.permutation(m))
+    shuffled_X = X[:, permutation]
+    shuffled_Y = Y[:, permutation].reshape((Y.shape[0],m))
+
+    # Step 2: Partition (shuffled_X, shuffled_Y). Minus the end case.
+    num_complete_minibatches = math.floor(m/mini_batch_size) # number of mini batches of size mini_batch_size in your partitionning
+    for k in range(0, num_complete_minibatches):
+        mini_batch_X = shuffled_X[:, k * mini_batch_size : k * mini_batch_size + mini_batch_size]
+        mini_batch_Y = shuffled_Y[:, k * mini_batch_size : k * mini_batch_size + mini_batch_size]
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+    
+    # Handling the end case (last mini-batch < mini_batch_size)
+    if m % mini_batch_size != 0:
+        mini_batch_X = shuffled_X[:, num_complete_minibatches * mini_batch_size : m]
+        mini_batch_Y = shuffled_Y[:, num_complete_minibatches * mini_batch_size : m]
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+    
+    return mini_batches
 
 def initialize_parameters():
                      
@@ -395,8 +342,7 @@ def forward_propagation(X, parameters):
 
 
     Y_hat = tf.concat([Z3, Z7], axis = 0)
-
-                                                
+                                 
     return Y_hat
 
 def compute_cost(Z3, Y, alpha = 0.8, threshold = 2.8):
@@ -406,14 +352,12 @@ def compute_cost(Z3, Y, alpha = 0.8, threshold = 2.8):
 
     huber_loss = tf.keras.losses.Huber(delta=threshold)
     t_cost = huber_loss(t, t_hat)
-    #  tf.cond(tf.norm(t - t_hat) < threshold, lambda: tf.squared_difference(t, t_hat), lambda : tf.norm(t - t_hat) - (0.5 * threshold))
 
     r_hat = Z3[:4]
     r = Y[:4]
 
     r_cost = tf.norm(r - (r_hat / tf.norm(r_hat, axis = 0)), axis = 0)
     cost = tf.reduce_mean(((1-alpha) * t_cost) + (alpha * r_cost))
-    #cost = tf.reduce_mean(tf.nn.cross_entropy_with_logits(logits = logits, labels = labels))
     
     return cost
 
@@ -430,6 +374,7 @@ def eval_accuracy(X, Y, Y_hat, X_train, Y_train, X_test, Y_test, t_treshold, r_t
 
   correct_rot = tf.cast(tf.less(angle_diff, [r_threshold]), "float")
   correct_trans = tf.cast(tf.less(tf.norm(Y[4:] - Y_hat[4:], axis = 0), [t_treshold]), "float")
+
   t_accuracy = tf.reduce_mean(tf.cast(correct_trans, "float"))
   r_accuracy = tf.reduce_mean(tf.cast(correct_rot, "float"))
   accuracy = tf.reduce_mean(tf.cast(correct_trans * correct_rot, "float"))
@@ -479,19 +424,21 @@ def pose_model(X_train, Y_train, X_test, Y_test, learning_rate = 0.001,
 
             epoch_cost = 0.
             
+            num_minibatches = int(m / minibatch_size) # number of minibatches of size minibatch_size in the train set
+            minibatches = random_mini_batches(X_train, Y_train, minibatch_size)
 
-            #for minibatch in minibatches:
+            for minibatch in minibatches:
 
-                #(minibatch_X, minibatch_Y) = minibatch
-                
-            _ , minibatch_cost = sess.run([optimizer, cost], feed_dict={X: X_train, Y: Y_train})
+                (minibatch_X, minibatch_Y) = minibatch
 
-                
-                #epoch_cost += minibatch_cost / minibatch_size
+                _ , minibatch_cost = sess.run([optimizer, cost], feed_dict={X: minibatch_X, Y: minibatch_Y})
+
+
+                epoch_cost += minibatch_cost / minibatch_size
 
             if print_cost == True and epoch % 100 == 0:
                 print ("Cost after epoch %i: %f" % (epoch, minibatch_cost))
-            if print_cost == True and epoch >= 1000 and epoch % 5 == 0:
+            if print_cost == True and epoch >= 500 and epoch % 5 == 0:
                 costs.append(minibatch_cost)
 
             saved_path = saver.save(sess, './pose-model')
@@ -518,6 +465,93 @@ def pose_model(X_train, Y_train, X_test, Y_test, learning_rate = 0.001,
         
         return parameters
 
+# Code largely pulled from this notebook:
+# https://www.kaggle.com/zstusnoopy/visualize-the-location-and-3d-bounding-box-of-car
+
+# convert euler angle to rotation matrix
+def euler_to_Rot(yaw, pitch, roll):
+    Y = np.array([[cos(yaw), 0, sin(yaw)],
+                  [0, 1, 0],
+                  [-sin(yaw), 0, cos(yaw)]])
+    P = np.array([[1, 0, 0],
+                  [0, cos(pitch), -sin(pitch)],
+                  [0, sin(pitch), cos(pitch)]])
+    R = np.array([[cos(roll), -sin(roll), 0],
+                  [sin(roll), cos(roll), 0],
+                  [0, 0, 1]])
+    return np.dot(Y, np.dot(P, R))
+
+def draw_line(image, points):
+    color = (255, 0, 0)
+    cv2.line(image, tuple(points[1][:2]), tuple(points[2][:2]), color, 4)
+    cv2.line(image, tuple(points[1][:2]), tuple(points[4][:2]), color, 4)
+
+    cv2.line(image, tuple(points[1][:2]), tuple(points[5][:2]), color, 4)
+    cv2.line(image, tuple(points[2][:2]), tuple(points[3][:2]), color, 4)
+    cv2.line(image, tuple(points[2][:2]), tuple(points[6][:2]), color, 4)
+    cv2.line(image, tuple(points[3][:2]), tuple(points[4][:2]), color, 4)
+    cv2.line(image, tuple(points[3][:2]), tuple(points[7][:2]), color, 4)
+
+    cv2.line(image, tuple(points[4][:2]), tuple(points[8][:2]), color, 4)
+    cv2.line(image, tuple(points[5][:2]), tuple(points[8][:2]), color, 4)
+
+    cv2.line(image, tuple(points[5][:2]), tuple(points[6][:2]), color, 4)
+    cv2.line(image, tuple(points[6][:2]), tuple(points[7][:2]), color, 4)
+    cv2.line(image, tuple(points[7][:2]), tuple(points[8][:2]), color, 4)
+    return image
+
+
+def draw_points(image, points):
+    image = np.array(image)
+    for (p_x, p_y, p_z) in points:
+        cv2.circle(image, (p_x, p_y), 5, (255, 0, 0), -1)
+    return image
+
+def visualize_poses(img, poses):
+  car_poses = []
+
+  for pose in poses:
+    quat = pose[:4]
+    quat = quat / np.linalg.norm(quat)
+    roll, pitch, yaw = quat2euler(*quat)
+
+    car_poses.append((yaw, pitch, roll, pose[4], pose[5], pose[6]))
+
+  x_l = 1.02
+  y_l = 0.80
+  z_l = 2.31
+  for yaw, pitch, roll, x, y, z in car_poses:
+      # I think the pitch and yaw should be exchanged
+      yaw, pitch, roll = -pitch, -yaw, -roll
+      Rt = np.eye(4)
+      t = np.array([x, y, z])
+      Rt[:3, 3] = t
+      Rt[:3, :3] = euler_to_Rot(yaw, pitch, roll).T
+      Rt = Rt[:3, :]
+      P = np.array([[0, 0, 0, 1],
+                    [x_l, y_l, -z_l, 1],
+                    [x_l, y_l, z_l, 1],
+                    [-x_l, y_l, z_l, 1],
+                    [-x_l, y_l, -z_l, 1],
+                    [x_l, -y_l, -z_l, 1],
+                    [x_l, -y_l, z_l, 1],
+                    [-x_l, -y_l, z_l, 1],
+                    [-x_l, -y_l, -z_l, 1]]).T
+
+      img_cor_points = np.dot(cam_matrix, np.dot(Rt, P))
+      img_cor_points = img_cor_points.T
+      img_cor_points[:, 0] /= img_cor_points[:, 2]
+      img_cor_points[:, 1] /= img_cor_points[:, 2]
+      img_cor_points = img_cor_points.astype(int)
+      img = draw_points(img, img_cor_points)
+      img = draw_line(img, img_cor_points)
+      
+  img = Image.fromarray(img)
+  plt.imshow(img)
+  plt.show()
+
+
+# ---------------------------------------- Running provided image through pre-trained model to display output  ---------------------------------------- #
 def detect(image_path, model_path):
   car_class_id = class_names.index('car')
   image = skimage.io.imread(image_path)
@@ -532,7 +566,7 @@ def detect(image_path, model_path):
   car_inputs = []
 
   for i in range(len(rois)):
-    if r['class_ids'][i] == car_class_id:
+    if r['class_ids'][i] == CAR_ID:
       y1,x1,y2,x2 = rois[i]
 
       # normalize
@@ -554,8 +588,6 @@ def detect(image_path, model_path):
       	car_inputs.append(tr_example)
 
   X = np.asarray(car_inputs).T
-  print(X)
-  print(X.shape)
 
   # Run through trained model
   poses = run_model(X, model_path)
@@ -582,6 +614,8 @@ def run_model(X_in, model_path):
     outputs = Y_hat.eval({X: X_in})
     print(outputs)
     return outputs
+
+# --- Main --- #
 
 def main():
   args = sys.argv[1:]
